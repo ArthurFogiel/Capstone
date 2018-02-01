e44753217403b7278375d7160987f8483b716fdb
Fogiel <afogiel@tainstruments.com>
2018-02-01 13:10:19 -0500

    Update ILoginViewModel and LoginView for a IsLoggedIn property

--- VisualStudioSolution/StockScreener/Interfaces/ILoginViewModel.cs
+++ VisualStudioSolution/StockScreener/Interfaces/ILoginViewModel.cs
@@ -11,6 +11,11 @@ namespace StockScreener.Interfaces
         string UserName { get; set; }
 
         /// <summary>
+        /// Do we have a user logged in?  Check if logged in user in the service is not null
+        /// </summary>
+        bool IsLoggedIn { get; set; }
+
+        /// <summary>
         /// Handle button press for logging in
         /// </summary>
         ICommand LoginCommand { get; }
